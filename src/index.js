95234749d00f2a5dae07eb5e7012f7e1a96daf1c
mhmdAljefri <algfry12@gmail.com>
2020-07-02 10:41:19 +0300

    init formik on submit example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Formik, Field, Form } from 'formik';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const validate = (values) => {
+  const errors = {};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
+function OnSubmitModalExample() {
+  const handleInValidForm = (errors) => alert(JSON.stringify(errors, null, 2));
+
+  const alertSuccess = (values) =>
+    alert(
+      `Wow, form submitted with values: ${JSON.stringify(values, null, 2)}`
+    );
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Formik
+        validate={validate}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={alertSuccess}
+      >
+        {({ isValid, errors, handleSubmit }) => (
+          <Form
+            onSubmit={(formProps) => {
+              if (!isValid) handleInValidForm(errors);
+              handleSubmit(formProps);
+            }}
+          >
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" placeholder="Jane" />
+
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" placeholder="Doe" />
+
+            <label htmlFor="email">Email</label>
+            <Field name="email" placeholder="jane@acme.com" type="email" />
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+ReactDOM.render(<OnSubmitModalExample />, document.getElementById('root'));
